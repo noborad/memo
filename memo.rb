ca7902496c2f2beda740951cb8e903846bb1a7b7
TokuhiroNoboru <tokujobn@gmail.com>
2020-12-25 19:59:12 +0900

    third commit

--- memo.rb
+++ memo.rb
@@ -22,7 +22,7 @@ if memo_type == 1
     end
 
 elsif memo_type == 2
-    puts "既存のメモの拡張子を除いたファイル名を入力してください"
+    puts "既存のメモの拡張子を含んだファイル名を入力してください"
     file_name = gets
     puts "追記したい内容を記入してください"
     puts "完了したらCtrl + Dをおします"
